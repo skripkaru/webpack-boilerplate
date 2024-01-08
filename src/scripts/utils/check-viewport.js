233cbd0c4e1bfd60d8b3d71b9cb7b4dfdf5f9c87
magical01 <denis.verzakov.015@gmail.com>
2024-01-08 17:48:08 +0500

    add utils and vars

---
+++ src/scripts/utils/check-viewport.js
@@ -0,0 +1,39 @@
+export const isMobilePlus = (width = 768) => {
+  const mediaQuery = window.matchMedia(`(min-width: ${width}px)`)
+
+  const handleChange = (e) => e.matches
+
+  mediaQuery.addListener(handleChange)
+
+  handleChange(mediaQuery)
+}
+
+export const isMobile = (width = 767) => {
+  const mediaQuery = window.matchMedia(`(max-width: ${width}px)`)
+
+  const handleChange = (e) => e.matches
+
+  mediaQuery.addListener(handleChange)
+
+  handleChange(mediaQuery)
+}
+
+export const isTabletPlus = (width = 1200) => {
+  const mediaQuery = window.matchMedia(`(min-width: ${width}px)`)
+
+  const handleChange = (e) => e.matches
+
+  mediaQuery.addListener(handleChange)
+
+  handleChange(mediaQuery)
+}
+
+export const isTablet = (width = 1199) => {
+  const mediaQuery = window.matchMedia(`(max-width: ${width}px)`)
+
+  const handleChange = (e) => e.matches
+
+  mediaQuery.addListener(handleChange)
+
+  handleChange(mediaQuery)
+}
---
+++ src/scripts/utils/disable-scroll.js
@@ -0,0 +1,18 @@
+import vars from '../vars'
+
+export const disableScroll = () => {
+  const fixBlocks = document?.querySelectorAll('.fixed-block')
+  const pagePosition = window.scrollY
+  const paddingOffset = `${window.innerWidth - vars.bodyEl.offsetWidth}px`
+
+  vars.htmlEl.style.scrollBehavior = null
+
+  fixBlocks.forEach((el) => {
+    el.style.paddingRight = paddingOffset
+  })
+
+  vars.bodyEl.style.paddingRight = paddingOffset
+  vars.bodyEl.classList.add('disable-scroll')
+  vars.bodyEl.dataset.position = pagePosition
+  vars.bodyEl.style.top = `-${pagePosition}px`
+}
